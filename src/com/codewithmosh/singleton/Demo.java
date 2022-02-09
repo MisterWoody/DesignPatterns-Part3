cd23d40dfecd5ccc8d55d9bffbb82aaa0d92f7cd
Tony Watkins <tony@llifion.net>
2022-02-09 14:10:25 +0000

    Refactor logger to use singleton pattern ensuring only a single instance can be created per filename - and show equivalence by sending output to console for loggers with both different and the same filename(s)

--- src/com/codewithmosh/singleton/Demo.java
+++ src/com/codewithmosh/singleton/Demo.java
@@ -2,9 +2,13 @@ package com.codewithmosh.singleton;
 
 public class Demo {
     public static void show() {
-        var logger = new Logger("LogFile.txt");
+        var logger = Logger.getInstance("LogFile.txt");
         System.out.println("Logging using the singleton pattern");
 
-        var logger2 = new Logger("LogFile.txt");
+        var logger2 = Logger.getInstance("LogFile1.txt");
+        var logger3 = Logger.getInstance("LogFile1.txt");
+
+        System.out.println(logger == logger2);
+        System.out.println(logger3 == logger2);
     }
 }
--- src/com/codewithmosh/singleton/Logger.java
+++ src/com/codewithmosh/singleton/Logger.java
@@ -1,13 +1,23 @@
 package com.codewithmosh.singleton;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Logger {
     private String fileName;
+    private static Map<String, Logger> instances = new HashMap<>();
 
-    public Logger(String fileName) {
+    private Logger(String fileName) {
         this.fileName = fileName;
     }
 
     public void write(String message) {
         System.out.println("Writing a message to the log.");
+    }
+
+    public static Logger getInstance(String filename) {
+        if (!instances.containsKey(filename))
+            instances.put(filename, new Logger(filename));
+        return instances.get(filename);
     }
 }
