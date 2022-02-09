0792db7c576b00a294a51a1c00326512b8300f83
Tony Watkins <tony@llifion.net>
2022-02-09 18:12:59 +0000

    Builder pattern exercise

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        com.codewithmosh.abstractFactory.Demo.show();
+        com.codewithmosh.builder.Demo.show();
     }
 }
