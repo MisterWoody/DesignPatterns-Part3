0118c45125358115275d61ac4221a1cebac78bfe
Tony Watkins <tony@llifion.net>
2022-02-09 12:55:44 +0000

    Initial implementation of our pseudo premier type application demonstrating issues with the current context menu that breaks the open closed principle - it needs to be amended for each new component type addition

--- src/com/codewithmosh/prototype/Demo.java
+++ src/com/codewithmosh/prototype/Demo.java
@@ -3,5 +3,12 @@ package com.codewithmosh.prototype;
 public class Demo {
     public static void show() {
         System.out.println("Got to the prototype pattern demo");
+
+        var timeline = new Timeline();
+        var text = new Text("Hello");
+        timeline.add(text);
+
+        var menu = new ContextMenu(timeline);
+        menu.duplicate(text);
     }
 }
