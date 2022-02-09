90f6fe7e5c6012f941ccc372735eadb0e5132623
Tony Watkins <tony@llifion.net>
2022-02-09 15:07:07 +0000

    Refactor our calendar class to an interface allowing the implementation to vary and have our factory method implemented in varying scheduler classes with a default implementation in the parent scheduler. Alternate implementations can create their own calendar objects as appropriate

--- src/com/codewithmosh/factory/Demo.java
+++ src/com/codewithmosh/factory/Demo.java
@@ -4,5 +4,9 @@ public class Demo {
     public static void show() {
         var scheduler = new Scheduler();
         scheduler.schedule(new Event());
+
+        // Arabian scheduler using an Arabian calendar
+        var arabianScheduler = new ArabianScheduler();
+        arabianScheduler.schedule(new Event());
     }
 }
--- src/com/codewithmosh/factory/Scheduler.java
+++ src/com/codewithmosh/factory/Scheduler.java
@@ -3,7 +3,11 @@ package com.codewithmosh.factory;
 import java.util.Date;
 
 public class Scheduler {
-    private Calendar calendar = new Calendar();
+    private Calendar calendar = createCalendar();
+
+    protected Calendar createCalendar() {
+        return new GregorianCalendar();
+    }
 
     public void schedule(Event event) {
         var today = new Date();
