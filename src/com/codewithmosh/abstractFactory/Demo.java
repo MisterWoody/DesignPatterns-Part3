2e1b2e9fee895519566609933345f5e2698ee36a
Tony Watkins <tony@llifion.net>
2022-02-09 15:43:50 +0000

    Introduce an abstract factory interface with associated methods to create a suitable workout and meal plan. Then exercise by setting the factory as a parameter for the homepage object and calling with our differing implementations

--- src/com/codewithmosh/abstractFactory/Demo.java
+++ src/com/codewithmosh/abstractFactory/Demo.java
@@ -3,6 +3,8 @@ package com.codewithmosh.abstractFactory;
 public class Demo {
     public static void show() {
         var homePage = new HomePage();
-        homePage.setGoal(Goal.WEIGHT_LOSS);
+        homePage.setGoal(new WeightLossFactory());
+
+        homePage.setGoal(new BuildMuscleFactory());
     }
 }
